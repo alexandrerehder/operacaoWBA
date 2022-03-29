5963639f7906013d7fa84cc53088eb1ade862861
Alexandre Rehder <alexandrerehder@hotmail.com>
2022-03-29 15:02:28 -0300

    Validação | Operação

--- src/main/java/br/wba/operacao/controller/OperacaoController.java
+++ src/main/java/br/wba/operacao/controller/OperacaoController.java
@@ -1,13 +1,22 @@
 package br.wba.operacao.controller;
 
-import br.wba.operacao.dto.OperacaoDTO;
-import br.wba.operacao.service.OperacaoService;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.wba.operacao.dto.OperacaoDTO;
+import br.wba.operacao.service.OperacaoService;
 
 @RestController
 @RequestMapping(value = "/operacao")
@@ -31,7 +40,7 @@ public class OperacaoController {
     }
 
     @PostMapping(value = "/cadastrar", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> cadastrarOperacao(@RequestBody OperacaoDTO dto) {
+    public ResponseEntity<?> cadastrarOperacao(@RequestBody @Valid OperacaoDTO dto) {
     	return new ResponseEntity<>(operacaoService.createOperacao(dto), HttpStatus.CREATED);	
     }
     
